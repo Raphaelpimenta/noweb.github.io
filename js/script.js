8f896f1aac53df0063f421d6fb4bd09f47284b16
Raphael Pimenta <raphael.pimentagomes@hotmail.com>
2023-08-21 05:06:06 -0300

    ajustes finais

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 import initMenuHamburguer from "./module/menuHamburguer.js";
 import initDropdownMenu from "./module/dropdown-menu.js";
 import initModal from "./module/modal.js";
+import initscrollSuave from "./module/scrollSuave.js";
+import initLinkMenu from "./module/ativaLink.js";
 
 initMenuHamburguer();
 initDropdownMenu();
 initModal();
+initscrollSuave();
+initLinkMenu();
