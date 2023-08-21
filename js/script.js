4b6cfb7e10193a9d59a7f3a9bc5a8932b7952661
Raphael Pimenta <raphael.pimentagomes@hotmail.com>
2023-08-21 05:18:39 -0300

    inserção de slide

--- js/script.js
+++ js/script.js
@@ -3,9 +3,17 @@ import initDropdownMenu from "./module/dropdown-menu.js";
 import initModal from "./module/modal.js";
 import initscrollSuave from "./module/scrollSuave.js";
 import initLinkMenu from "./module/ativaLink.js";
+import Slide from "./module/slide.js";
 
 initMenuHamburguer();
 initDropdownMenu();
 initModal();
 initscrollSuave();
 initLinkMenu();
+
+const slide = new Slide('.slide', '.slide-wrapper');
+slide.init();
+console.log(slide);
+
+slide.changeSlide(3);
+slide.activePrevSlide();
