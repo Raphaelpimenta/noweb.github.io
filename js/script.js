9e44606b939a7abdba17e86941f0e68b5bf39ab8
Raphael Pimenta <raphael.pimentagomes@hotmail.com>
2023-08-21 05:37:49 -0300

    ajustes finais

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ import initDropdownMenu from "./module/dropdown-menu.js";
 import initModal from "./module/modal.js";
 import initscrollSuave from "./module/scrollSuave.js";
 import initLinkMenu from "./module/ativaLink.js";
-import Slide from "./module/slide.js";
+// import Slide from "./module/slide.js";
 
 initMenuHamburguer();
 initDropdownMenu();
@@ -11,9 +11,7 @@ initModal();
 initscrollSuave();
 initLinkMenu();
 
-const slide = new Slide('.slide', '.slide-wrapper');
-slide.init();
-console.log(slide);
-
-slide.changeSlide(3);
-slide.activePrevSlide();
+// const slide = new Slide('.slide', '.slide-wrapper');
+// slide.init();
+// slide.changeSlide(3);
+// slide.activePrevSlide();
